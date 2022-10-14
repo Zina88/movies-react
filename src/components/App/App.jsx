a2b0e75d1d140f4383082f786d46f0618f25cec5
Zina <zinasaenko@hotmail.com>
2022-10-15 01:16:10 +0300

    create pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 import MoviesPage from 'pages/MoviesPage';
