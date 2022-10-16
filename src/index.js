3a95e722357e0b27058fbe6a382bfc8126577419
Zina <zinasaenko@hotmail.com>
2022-10-16 11:36:58 +0300

    create  basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <BrowserRouter basename="/goit-react-hw-05-movies/"> */}
-    <BrowserRouter>
+    <BrowserRouter basename="goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
