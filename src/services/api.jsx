0891d407f8b09b84ac29803106a59004bc01b166
Zina <zinasaenko@hotmail.com>
2023-01-04 22:20:12 +0200

    fix

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,5 +1,8 @@
-const API_KEY = process.env.REACT_APP_KEY;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const API_KEY = process.env.REACT_APP_KEY;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const API_KEY = '8562b39677dad16e2334fc338fdc606e';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const getTranding = async currentPage => {
 	const response = await fetch(
